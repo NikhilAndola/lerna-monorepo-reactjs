4a22af740bef0ecadfc9e5c380c5fda7ddf69d22
nikhil andola <nikhilandola123@gmail.com>
2022-05-13 10:25:19 +0530

    Update index.js

--- packages/server-api/index.js
+++ packages/server-api/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const port = process.env.PORT || 8000;
+ const express = require("express")
+ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("I am a backend server");
-});
+})
 
-app.listen(port, (err)=> {
-    if (err) {
-        console.log(`Error: ${err.message}`); 
-    } else {
-        console.log(`Server is listening on port ${port}`);
-    }
-    }
-);
+
+ app.listen(port, (err) => {
+     if(err){
+         console.log(`Error:  ${err.message}`)
+     } else {
+         console.log(`Listening on port ${port}`)
+     }
+ })
